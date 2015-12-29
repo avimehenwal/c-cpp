9b5e58c46204418bc9108d90ce20a49025a058eb
avimehenwal <avi.mehanwal@gmail.com>
2015-12-29 22:13:41 +0530

    sizeof C types

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+ *   hello.c
+ *   c.avimehenwal
+ * 
+ *   Created by avimehenwal on 29/12/15.
+ *   Copyright (c) 2015 avimehenwal. All rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+
+    return 0;
+}
---
+++ sizeof_types.c
@@ -0,0 +1,19 @@
+/*
+ *   sizeof_types.c
+ *   c.avimehenwal
+ * 
+ *   Created by avimehenwal on 29/12/15.
+ *   Copyright (c) 2015 avimehenwal. All rights reserved.
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("sizeof(char)      == %d\n", sizeof(char));
+    printf("sizeof(short)     == %d\n", sizeof(short));
+    printf("sizeof(int)       == %d\n", sizeof(int));
+    printf("sizeof(long)      == %d\n", sizeof(long));
+    printf("sizeof(long long) == %d\n", sizeof(long long));
+
+    return 0;
+}
